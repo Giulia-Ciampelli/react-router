e604507fe8e4a3c26b22709e23a693be41373d52
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-26 15:57:49 +0100

    docs: fill SinglePost page and link data

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,10 +1,57 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 // stile
-// import style from "../components/Main.modules.css";
+import style from "../components/Main.module.css";
 
 export default function SinglePost() {
-    return (
-        <p>
-            post singolo
+    const { slug } = useParams();
+    const [post, setPost] = useState();
+    const url = `http://localhost:3000/posts/${slug}`;
+
+    // funzione fetch
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setPost(data.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [slug])
+
+    // condizionale per slug non trovato
+    if (!post) {
+        return <p>
+            Post non trovato
         </p>
+    }
+
+    return (
+        <div className={style.card}>
+            <h2>
+                {post.title}
+            </h2>
+            <img src={`http://localhost:3000/${post.image}`} alt={post.title} />
+            <p>
+                {post.content}
+            </p>
+            <p>
+                {post.category}
+            </p>
+            <div className={style.tags}>
+                <h2>
+                    Tags:
+                </h2>
+                <p>
+                    {Array.isArray(post.tags) && post.tags.length > 0 ? post.tags.join(', ') : 'No tags available'}
+                </p>
+            </div>
+            <p>
+                {post.public ? 'Post pubblico' : 'Post privato'}
+            </p>
+        </div>
     )
 }
